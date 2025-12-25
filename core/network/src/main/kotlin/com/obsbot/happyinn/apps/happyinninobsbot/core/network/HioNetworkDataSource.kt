7c91e818c66518923fcc28b38d9a390d5ff3e90f
happyinn <19992662691@163.com>
2025-12-25 14:44:44 +0800

    添加几个中文注释

--- core/network/src/main/kotlin/com/obsbot/happyinn/apps/happyinninobsbot/core/network/HioNetworkDataSource.kt
+++ core/network/src/main/kotlin/com/obsbot/happyinn/apps/happyinninobsbot/core/network/HioNetworkDataSource.kt
@@ -14,6 +14,10 @@
 
 package com.obsbot.happyinn.apps.happyinninobsbot.core.network
 
+import com.obsbot.happyinn.apps.happyinninobsbot.core.network.model.NetworkChangeList
+import com.obsbot.happyinn.apps.happyinninobsbot.core.network.model.NetworkNewsResource
+import com.obsbot.happyinn.apps.happyinninobsbot.core.network.model.NetworkTopic
+
 
 /**
  * 表示与NIA后端网络调用的接口
@@ -25,7 +29,7 @@ interface HioNetworkDataSource {
      * @param ids 可选的主题ID列表，用于过滤结果
      * @return 网络主题列表
      */
-//    suspend fun getTopics(ids: List<String>? = null): List<NetworkTopic>
+    suspend fun getTopics(ids: List<String>? = null): List<NetworkTopic>
 
     /**
      * 获取新闻资源列表
@@ -33,7 +37,7 @@ interface HioNetworkDataSource {
      * @param ids 可选的新闻资源ID列表，用于过滤结果
      * @return 网络新闻资源列表
      */
-//    suspend fun getNewsResources(ids: List<String>? = null): List<NetworkNewsResource>
+    suspend fun getNewsResources(ids: List<String>? = null): List<NetworkNewsResource>
 
     /**
      * 获取主题变更列表
@@ -41,7 +45,7 @@ interface HioNetworkDataSource {
      * @param after 可选的时间戳，用于获取在此之后的变更
      * @return 网络变更列表
      */
-//    suspend fun getTopicChangeList(after: Int? = null): List<NetworkChangeList>
+    suspend fun getTopicChangeList(after: Int? = null): List<NetworkChangeList>
 
     /**
      * 获取新闻资源变更列表
@@ -49,5 +53,5 @@ interface HioNetworkDataSource {
      * @param after 可选的时间戳，用于获取在此之后的变更
      * @return 网络变更列表
      */
-//    suspend fun getNewsResourceChangeList(after: Int? = null): List<NetworkChangeList>
+    suspend fun getNewsResourceChangeList(after: Int? = null): List<NetworkChangeList>
 }
